682660b6b708a154a7bcbf99417dcb59798d2539
BadLogic1618 <benjohnspencer@gmail.com>
2023-12-02 10:44:56 -0800

    upgraded to faster algorithm with help from 'smplante' https://github.com/smplante/advent-of-code/tree/main

---
+++ day_01/benches/day_1_benchmark.rs
@@ -0,0 +1,22 @@
+// use day_01::{
+//     part_one,
+//     part_two,
+//     INPUT,
+// };
+// use criterion::{
+//     criterion_group,
+//     criterion_main,
+//     Criterion
+// };
+
+// fn bench_part_one(c: &mut Criterion) {
+//     c.bench_function("part1", |b|b.iter(||part_one(&INPUT)));
+// }
+
+// fn bench_part_two(c: &mut Criterion) {
+//     c.bench_function("part2", |b|b.iter(||part_two(&INPUT)));
+    
+// }
+
+// criterion_group!(all, bench_part_one, bench_part_two);
+// criterion_main!(all);
--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,16 +1,16 @@
-const INPUT: &'static str = include_str!("input.txt");
+pub const INPUT: &'static str = include_str!("input.txt");
 
 // including first and last characters of orignal str in replacement because some values in input have overlapping start/end characters
-const REPLACEMENT_PAIRS: [(&'static str, &'static str); 9] = [
-    ("one", "o1e"),
-    ("two", "t2o"),
-    ("three", "t3e"),
-    ("four", "f4r"),
-    ("five", "f5e"),
-    ("six", "s6x"),
-    ("seven", "s7n"),
-    ("eight", "e8t"),
-    ("nine", "n9e"),
+const REPLACEMENT_PAIRS: [(&[u8], u8); 9] = [
+    (b"one", 1),
+    (b"two", 2),
+    (b"three", 3),
+    (b"four", 4),
+    (b"five", 5),
+    (b"six", 6),
+    (b"seven", 7),
+    (b"eight", 8),
+    (b"nine", 9),
 ];
 
 fn main() {
@@ -18,37 +18,56 @@ fn main() {
     println!("Answer for part 2: {0}", part_two(INPUT));
 }
 
-fn part_one(input: &str) -> u32 {
+pub fn part_one(input: &str) -> u8 {
     input.lines()
-        .map(|val| line_to_calibration_val(val))
+        .map(line_to_calibration_val)
         .sum()
 }
 
-fn line_to_calibration_val(input: &str) -> u32 {
-    let output = format!(
-        "{}{}",
-        first_numeric_char(&mut input.chars()),
-        first_numeric_char(&mut input.chars().rev())
-    );
-    
-    output.parse::<u32>()
-        .expect("could not parse value")
+fn line_to_calibration_val(input: &str) -> u8 {
+    let input = input.as_bytes();
+
+    let first_digit = input.iter().find(|byte| byte.is_ascii_digit()).unwrap();
+    let second_digit = input.iter().rfind(|byte| byte.is_ascii_digit()).unwrap();
+    first_digit * 10 + second_digit
 }
 
-fn first_numeric_char(input: &mut impl Iterator<Item=char>) -> char {
-    input.find(|char| char.is_numeric())
-        .expect("could not find digit")
+pub fn part_two(input: &str) -> u8 {
+    input.lines()
+        .map(line_to_calibration_val_words)
+        .sum()
 }
 
-fn part_two(input: &str) -> u32 {
-    let input = preprocess_input(input);
-    part_one(&input)
-}
+fn line_to_calibration_val_words(line: &str) -> u8 {
+    let input = line.as_bytes();
+    let mut n = 0;
 
-fn preprocess_input(input: &str) -> String {
-    let mut input = input.to_owned();
-    for pair in REPLACEMENT_PAIRS {
-        input = input.replace(pair.0, pair.1);
+    'outer: for i in 0..input.len() {
+        if input[i].is_ascii_digit() {
+            n += (input[i] - b'0') * 10;
+            break;
+        }
+
+        for val in REPLACEMENT_PAIRS {
+            if input[i..].starts_with(val.0){
+                n += val.1 * 10;
+                break 'outer;
+            }
+        }
     }
-    input
+
+    'outer: for i in input.len()-1..=0{
+        if input[i].is_ascii_digit() {
+            n += input[i] - b'0';
+            break;
+        }
+        for val in REPLACEMENT_PAIRS {
+            if input[..i].ends_with(val.0){
+                n += val.1;
+                break 'outer;
+            }
+        }
+    }
+
+    n
 }
