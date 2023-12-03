565cce29ad28cfd9c941cb82d42fea77c7a75b2f
BadLogic1618 <benjohnspencer@gmail.com>
2023-12-03 12:05:02 -0800

    day 03 part 2 initial solution

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -83,9 +83,69 @@ fn part_two(input: &str) -> usize {
     for row in 0..BOUND {
         for col in 0..BOUND {
             if input[row][col] != b'*' { continue; }
-            
+            if let Some(locations) = get_valid_neighbouring_part_nums(row, col, &input) {
+                let num_1 = scan_for_num_at(locations[0].0, locations[0].1, &input);
+                let num_2 = scan_for_num_at(locations[1].0, locations[1].1, &input);
+                sum += num_1 * num_2;
+            }
         }
     }
 
-    0
+    sum
+}
+
+fn get_valid_neighbouring_part_nums(row: usize, col: usize, input: &[&[u8]]) -> Option<Vec<(usize, usize)>> {
+    
+    let mut part_num_locations = scan_row_for_part_num(row-1, col, input);
+
+    if col > 0 && input[row][col-1].is_ascii_digit() {
+        part_num_locations.push((row, col-1));
+    }
+
+    if col < BOUND-1 && input[row][col+1].is_ascii_digit() {
+        part_num_locations.push((row, col+1))
+    }
+
+    part_num_locations.append(&mut scan_row_for_part_num(row+1, col, input));
+
+    match part_num_locations.len() {
+        2 => Some(part_num_locations),
+        _ => None,
+    }
+}
+
+fn scan_row_for_part_num(row: usize, col: usize, input: &[&[u8]]) -> Vec<(usize, usize)> {
+    let mut locations = Vec::new();
+
+    if row > BOUND-1 { return locations; }
+
+    let mut last_char = b'.';
+    for i in col.max(1)-1..=(col+1).min(BOUND) {
+        let next_char = input[row][i];
+        if next_char.is_ascii_digit() && !last_char.is_ascii_digit() {
+            locations.push((row, i))
+        }
+        last_char = next_char;
+    }
+
+    locations
+}
+
+fn scan_for_num_at(row: usize, col:usize, input: &[&[u8]]) -> usize {
+    let mut sum = 0;
+    
+    let mut i = 0;
+    while i <= col && input[row][col-i].is_ascii_digit() {
+        sum += 10_usize.pow(i as u32) * (input[row][col-i] - b'0') as usize;
+        i += 1;
+    }
+
+    i = 1;
+    while (col+i) < BOUND && input[row][col+i].is_ascii_digit() {
+        sum *= 10;
+        sum += (input[row][col+i] - b'0') as usize;
+        i += 1;
+    }
+
+    sum
 }
