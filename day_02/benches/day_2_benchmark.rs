cff76f50fef960aa191d201deb5beceb36242c3e
BadLogic1618 <benjohnspencer@gmail.com>
2023-12-02 17:19:32 -0800

    doing some setup to be able to bench day 02

---
+++ day_02/benches/day_2_benchmark.rs
@@ -0,0 +1,19 @@
+// use day_02::{part1::part_one, part2::part_two};
+
+// use criterion::{
+//     criterion_group,
+//     criterion_main,
+//     Criterion
+// };
+
+// fn bench_part_one(c: &mut Criterion) {
+//     c.bench_function("part1", |b|b.iter(||part_one()));
+// }
+
+// fn bench_part_two(c: &mut Criterion) {
+//     c.bench_function("part2", |b|b.iter(||part_two()));
+
+// }
+
+// criterion_group!(all, bench_part_one, bench_part_two);
+// criterion_main!(all);
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,9 +1,10 @@
 mod input;
-mod part1;
-mod part2;
+pub mod part1;
+pub mod part2;
 
 use part1::*;
 use part2::*;
+
 fn main() {
     println!("Answer for part one: {}", part_one());
     println!("Answer for part one: {}", part_two());
